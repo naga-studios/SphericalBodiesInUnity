a1febeb44d4ecd4d7cf83c2321b072b7c7529097
D <dheerajk.har@gmail.com>
2019-06-17 16:46:24 -0700

    Add files via upload

---
+++ PlayerCameraScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraScript : MonoBehaviour
+{
+    [SerializeField] private float cameraSensitivity = 5f;
+
+    [SerializeField] private float yMin = 5f;
+    [SerializeField] private float yMax = 5f;
+
+    [SerializeField] private Transform player;
+
+    private Transform cameraTransform;
+    private Vector2 currentPos;
+    private Vector2 newPos;
+
+    private float verticalLookRotation;
+
+    void Start()
+    {
+        cameraTransform = this.transform;
+    }
+
+    void Update()
+    {
+        CheckForMouseMovement();
+    }
+
+    private void CheckForMouseMovement()
+    {
+        player.Rotate(Vector3.up * Input.GetAxis("Mouse X") * cameraSensitivity);
+        verticalLookRotation += Input.GetAxis("Mouse Y") * cameraSensitivity;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, yMin, yMax);
+        cameraTransform.localEulerAngles = Vector3.left * verticalLookRotation;
+    }
+}
---
+++ PlayerScript.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (ConstantForce))]
+[RequireComponent(typeof (Rigidbody))]
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] private GameObject world;
+    [SerializeField] private float fauxGravityForce = 5f;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 20f;
+    [SerializeField] private float worldRadius = 20f;
+
+    private bool isGrounded = false;
+    private float verticalMovement = 0f;
+
+    private ConstantForce playerConstantForceComponent;
+    private Vector3 worldCenter;
+    private Rigidbody playerRigidbody;
+    private Transform playerTransform;
+
+    void Start()
+    {
+        worldCenter = world.transform.position;
+        playerConstantForceComponent = this.GetComponent<ConstantForce>();
+        playerRigidbody = this.GetComponent<Rigidbody>();
+        playerTransform = this.GetComponent<Transform>();
+        //worldRadius = world.GetComponent<MeshRenderer>().bounds.extents.magnitude;
+    }
+
+    void FixedUpdate()
+    {
+        playerRigidbody.velocity = Vector3.zero;
+        playerRigidbody.angularVelocity = Vector3.zero;
+    }
+
+    void Update()
+    {
+        HandlePlayerMovement();
+        ArtificialGravitationalPull();
+        AdjustBodyWhileInAir();
+    }
+
+    private void HandlePlayerMovement()
+    {
+        verticalMovement = (Input.GetButtonDown("Jump")) ? 1f : 0f;
+        Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), verticalMovement, Input.GetAxisRaw("Vertical")).normalized;
+        moveDir.x *= moveSpeed * Time.deltaTime;
+        moveDir.z *= moveSpeed * Time.deltaTime;
+        transform.Translate(moveDir);
+    }
+
+    private void ArtificialGravitationalPull()
+    {
+        Vector3 upDirection = (playerTransform.position - worldCenter).normalized;
+
+        playerTransform.rotation = Quaternion.FromToRotation(playerTransform.up, upDirection) * playerTransform.rotation;
+
+        if(GetDistanceBetweenSurfaceAndPlayer() > 0.7f)
+        {
+            playerTransform.Translate(new Vector3(0, -fauxGravityForce * Time.deltaTime, 0));
+            isGrounded = false;
+        }
+        else
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void AdjustBodyWhileInAir()
+    {
+        if (!isGrounded)
+        {
+            Vector3 upDirection = (playerTransform.position - worldCenter).normalized;
+            float distance = GetDistanceBetweenSurfaceAndPlayer();
+            if(distance > 2)
+            {
+                playerTransform.rotation = Quaternion.Lerp(playerTransform.rotation, Quaternion.FromToRotation(playerTransform.up, upDirection) * playerTransform.rotation, 1f * Time.deltaTime);
+            }
+            else
+            {
+                isGrounded = true;
+            }
+        }
+    }
+
+    private float GetDistanceBetweenSurfaceAndPlayer()
+    {
+        float distance = Vector3.Distance(playerTransform.position, worldCenter) - worldRadius;
+        return distance;
+    }
+}
